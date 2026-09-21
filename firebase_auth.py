c07bad44d39d84e91c53d6983c38e479dd2e59e4
66050907 <66050907@kmitl.ac.th>
2026-09-21 10:58:37 +0700

    Update firebase_auth.py

--- firebase_auth.py
+++ firebase_auth.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("FIREBASE_API_KEY", "").strip()
+API_KEY = os.getenv("FIREBASE_API_KEY", "AIzaSyC86L3hLkpBz9eGHEqKw3TX23HOqZpYnAI").strip()
 BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts"
 
 ERROR_MESSAGES = {
